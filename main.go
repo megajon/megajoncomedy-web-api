e44d5c0dbcc8fc46a01035f269440abf71ccd987
megajon <jonathan.seubert@megajon.com>
2023-03-29 22:27:20 -0700

    adds getAlbums route handler

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
 func main() {
-	port := os.Getenv("PORT")
+	// port := os.Getenv("PORT")
+	port := "3000"
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -21,9 +34,26 @@ func main() {
 	// router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Welcome to the megajon-web api")
-	})
+	router.GET("/", getRoot)
+	router.GET("/albums", getAlbums)
 
 	router.Run(":" + port)
+}
+
+func getRoot(c *gin.Context) {
+	c.String(http.StatusOK, "Welcome to the megajon-web api")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
